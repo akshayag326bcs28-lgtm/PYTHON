c1ed5b32a3302a6ced02e38d6a0d3dba069a934b
Akshaya G <akshayag326_bcs28@mepcoeng.ac.in>
2026-04-16 22:55:33 +0530

    Add count_logins.py to count login dates

---
+++ COLLECTIONS/count_logins.py
@@ -0,0 +1,6 @@
+from collections import Counter
+from datetime import datetime
+logins = [ "2026-03-10 10:00", "2026-03-10 12:00", "2026-03-11 09:30", "2026-03-11 11:45", "2026-03-11 14:00" ]
+dates = [datetime.strptime(ts, "%Y-%m-%d %H:%M").date() for ts in logins]
+counter = Counter(dates)
+print(counter)
