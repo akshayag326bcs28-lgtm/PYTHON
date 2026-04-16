5d96c1ca44da79b4fc16dbd037390bb0ddbdcb27
Akshaya G <akshayag326_bcs28@mepcoeng.ac.in>
2026-04-16 23:13:14 +0530

    Create area.py

---
+++ CLASSES/area.py
@@ -0,0 +1,9 @@
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+rect = Rectangle(10, 5)
+print("The area is:",rect.area())
